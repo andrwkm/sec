91c6ffb11e1e70135525ef05bb119d2d6eeab951
andrwkm <94572878+andrwkm@users.noreply.github.com>
2025-12-22 16:51:00 -0500

    Initial commit: first anniversary website
    Add all source files for a personal website commemorating a first anniversary, including HTML, CSS, JavaScript for interactive features (typewriter effect, date counter, image slideshow, animated background), images, and background music. Includes a README with project overview and file structure.

---
+++ js/typeWriter.js
@@ -0,0 +1,36 @@
+
+let i = 0;
+let text1 = "Dear Princess Pooka Plum (PPP)";
+let text2 = "Happy 2 Years Together!"
+let speed = 100;
+
+function typeWriter(text, para){
+	if(ok == 2){
+		clearInterval(typeInterval);
+	}
+	if(i < text.length){
+		document.getElementById(para).innerHTML += text.charAt(i);
+		i++;
+		speed = Math.random() * 50 + 100;
+	}
+	else{
+		if(ok == 0){
+			i = 0;
+		}
+		ok += 1;
+	}
+}
+
+var typeInterval;
+
+//window.onload = function() {
+//	window.onload = function(){};
+   	typeInterval = setInterval(function(){
+		if(ok == 0){
+			typeWriter(text1, "txt1");
+		}
+		else if(ok == 1){
+			typeWriter(text2, "txt2");
+		}
+	}, 100);
+//};
